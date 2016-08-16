e164d8027c9e22a007fc86a7664652f9e4f6417d
Chester Ismay <chester.ismay@gmail.com>
2016-08-16 16:12:59 -0700

    Added gitbook (need to add preambles) and raw Word and epub formats

--- R/thesis.R
+++ R/thesis.R
@@ -13,7 +13,6 @@
 #'  output: thesisdown::thesis_pdf
 #' }
 thesis_pdf <- function(toc = TRUE, toc_depth = 3){
-  #template <- system.file("template.tex", package = )
 
   base <- bookdown::pdf_book(template = "template.tex",
     toc = toc,
@@ -25,9 +24,85 @@ thesis_pdf <- function(toc = TRUE, toc_depth = 3){
   # Mostly copied from knitr::render_sweave
   base$knitr$opts_chunk$comment <- NA
   base$knitr$opts_chunk$fig.align <- "center"
+  # Not sure if needed?
   base$knitr$knit_hooks$plot <- knitr:::hook_plot_tex
 
   base
-  #  }
+
+}
+
+#' Creates an R Markdown gitbook Thesis document
+#'
+#' This is a function called in output in the YAML of the driver Rmd file
+#' to specify the creation of a webpage version of the thesis.
+#'
+#' @export
+#' @return A gitbook webpage
+#' @examples
+#' \dontrun{
+#'  output: thesisdown::thesis_gitbook
+#' }
+thesis_gitbook <- function(){
+
+  base <- bookdown::gitbook(
+    split_by = "chapter+number",
+    config = list(toc = list(collapse = "section",
+      before = '<li><a href="./"></a></li>',
+      after = '<li><a href="https://github.com/rstudio/bookdown" target="blank">Published with bookdown</a></li>')
+    )
+  )
+
+  # Mostly copied from knitr::render_sweave
+  base$knitr$opts_chunk$comment <- NA
+  base$knitr$opts_chunk$fig.align <- "center"
+
+  base
+
+}
+
+#' Creates an R Markdown Word Thesis document
+#'
+#' This is a function called in output in the YAML of the driver Rmd file
+#' to specify the creation of a Microsoft Word version of the thesis.
+#'
+#' @export
+#' @return A Word Document based on (hopefully soon, but not currently)
+#' the Reed Senior Thesis Word template
+#' @examples
+#' \dontrun{
+#'  output: thesisdown::thesis_word
+#' }
+thesis_word <- function(){
+
+  base <- bookdown::word_document2()
+
+  # Mostly copied from knitr::render_sweave
+  base$knitr$opts_chunk$comment <- NA
+  base$knitr$opts_chunk$fig.align <- "center"
+
+  base
+
+}
+
+#' Creates an R Markdown epub Thesis document
+#'
+#' This is a function called in output in the YAML of the driver Rmd file
+#' to specify the creation of a epub version of the thesis.
+#'
+#' @export
+#' @return A ebook version of the thesis
+#' @examples
+#' \dontrun{
+#'  output: thesisdown::thesis_epub
+#' }
+thesis_epub <- function(){
+
+  base <- bookdown::epub_book()
+
+  # Mostly copied from knitr::render_sweave
+  base$knitr$opts_chunk$comment <- NA
+  base$knitr$opts_chunk$fig.align <- "center"
+
+  base
 
 }
