38a85988dc1647c64fb57224cd85a3888c6c07f6
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-09-15 12:06:28 -0700

    update thesis_pdf to allow for extra arguments

--- R/thesis.R
+++ R/thesis.R
@@ -4,22 +4,27 @@
 #' to specify using the Reed College Senior Thesis LaTeX template and cls files.
 #'
 #' @export
+#'
 #' @param toc A Boolean (TRUE or FALSE) specifying where table of contents should be created
 #' @param toc_depth A positive integer
+#' @param ... arguments to be passed to \code{rmarkdown::\link[rmarkdown]{pdf_document}}
+#'
 #' @return A modified \code{pdf_document} based on the Reed Senior Thesis LaTeX
 #'   template
+#' @note The arguments highlight, keep_tex, and pandoc_args, are already set.
 #' @examples
 #' \dontrun{
 #'  output: thesisdown::thesis_pdf
 #' }
-thesis_pdf <- function(toc = TRUE, toc_depth = 3){
+thesis_pdf <- function(toc = TRUE, toc_depth = 3, ...){
 
   base <- bookdown::pdf_book(template = "template.tex",
     toc = toc,
     toc_depth = toc_depth,
     highlight = "pygments",
     keep_tex = TRUE,
-    pandoc_args = "--chapters")
+    pandoc_args = "--chapters",
+    ...)
 
   # Mostly copied from knitr::render_sweave
   base$knitr$opts_chunk$comment <- NA
