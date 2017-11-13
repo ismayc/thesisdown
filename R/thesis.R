4b69ad30667408c5ee66bcdcdcb183201384dcd6
Manuel Vogel <mavogel@posteo.de>
2017-11-13 17:19:21 +0100

    Fixed pandoc 2.0 chapters flag. (#34)
    Thanks, @mavogel!

--- R/thesis.R
+++ R/thesis.R
@@ -20,7 +20,7 @@ thesis_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default", ...){
     toc_depth = toc_depth,
     highlight = highlight,
     keep_tex = TRUE,
-    pandoc_args = "--chapters",
+    pandoc_args = "--top-level-division=chapter",
     ...)
 
   # Mostly copied from knitr::render_sweave
