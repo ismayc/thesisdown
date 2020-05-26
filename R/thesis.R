9bf78a3be301bcca0e9d7674e3259522dca268f7
Chester Ismay <chester.ismay@gmail.com>
2020-05-26 06:11:48 -0700

    Develop (#89)
    * Tweak phrasing
    
    * Updates (#68)
    
    * Add hadamardown template (#67)
    
    * Add hadamardown
    
    * Update documentation
    
    * Update DESCRIPTION
    
    Co-authored-by: Antoine Bichat <30300749+abichat@users.noreply.github.com>
    
    * update (#88)
    
    * Add hadamardown template (#67)
    
    * Add hadamardown
    
    * Update documentation
    
    * Update DESCRIPTION
    
    * Switch to PDF by default
    
    * Update README.md
    
    * Update README.md
    
    * updates README.md (#78)
    
    * updates README.md
    
    * changes dependencies from devtools to remotes.
    
    * Update template.tex (#79)
    
    * Add link to amsterdown pkg in README (#80)
    
    * Fixed broken URLs and added few more (#85)
    
    * Fixed broken URLs and added few more
    
    * Reversed a URL change for markdown cheatsheet
    
    I moved the Rmakrdown Cheatsheet under R Markdown and reversed the modification of the Markdown Cheatsheet.
    
    * Allow user to change bookdown::gitbook() options (#84)
    
    * Add ...
    
    * Allow user to change "before" and "after" toc arguments
    
    * Pass all type of option and sub-options to bookdown::gitbook
    
    Co-authored-by: Antoine Bichat <30300749+abichat@users.noreply.github.com>
    Co-authored-by: Leon Reteig <35737105+lcreteig@users.noreply.github.com>
    Co-authored-by: Mehrad Mahmoudian <m.mahmoudian@gmail.com>
    
    * Fix Package Fancyhdr Warning: \headheight is too small (12.0pt):
    Make it at least 14.49998pt.
    
    * Fix abstract messing up bib findings/fix spacing
    
    * Move preliminary content to their own folder
    
    * Start using {here}
    
    * Apply {styler} / Can't use rmd_files with {thesisdown} in _bookdown.yml
    
    * Remove extra line
    
    Co-authored-by: Chester Ismay <chester.ismay@datarobot.com>
    Co-authored-by: Antoine Bichat <30300749+abichat@users.noreply.github.com>
    Co-authored-by: Leon Reteig <35737105+lcreteig@users.noreply.github.com>
    Co-authored-by: Mehrad Mahmoudian <m.mahmoudian@gmail.com>

--- R/thesis.R
+++ R/thesis.R
@@ -5,37 +5,40 @@
 #'
 #' @export
 #'
-#' @param toc A Boolean (TRUE or FALSE) specifying whether table of contents should be created
+#' @param toc A Boolean (TRUE or FALSE) specifying whether table of contents
+#'  should be created
 #' @param toc_depth A positive integer
 #' @param ... Further arguments passed to or from other methods.
-#' @param highlight Syntax highlighting style. Supported styles include "default", "tango", "pygments", "kate", "monochrome", "espresso", "zenburn", and "haddock". Pass NULL to prevent syntax highlighting.
+#' @param highlight Syntax highlighting style. Supported styles include
+#' "default", "tango", "pygments", "kate", "monochrome", "espresso", "zenburn",
+#' and "haddock". Pass NULL to prevent syntax highlighting.
 #'
 #' @return A modified \code{pdf_document} based on the Reed Senior Thesis LaTeX
 #'   template
 #' @examples
 #' \dontrun{
-#'  output: thesisdown::thesis_pdf
+#' output:thesisdown::thesis_pdf
 #' }
-thesis_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default", ...){
-
-  base <- bookdown::pdf_book(template = "template.tex",
+thesis_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default", ...) {
+  base <- bookdown::pdf_book(
+    template = "template.tex",
     toc = toc,
     toc_depth = toc_depth,
     highlight = highlight,
     keep_tex = TRUE,
     pandoc_args = "--top-level-division=chapter",
-    ...)
+    ...
+  )
 
   # Mostly copied from knitr::render_sweave
   base$knitr$opts_chunk$comment <- NA
-  #base$knitr$opts_chunk$fig.align <- "center"
+  # base$knitr$opts_chunk$fig.align <- "center"
 
   old_opt <- getOption("bookdown.post.latex")
   options(bookdown.post.latex = fix_envs)
   on.exit(options(bookdown.post.late = old_opt))
 
   base
-
 }
 
 #' Creates an R Markdown gitbook Thesis document
@@ -49,14 +52,19 @@ thesis_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default", ...){
 #' @return A gitbook webpage
 #' @examples
 #' \dontrun{
-#'  output: thesisdown::thesis_gitbook
+#' output:thesisdown::thesis_gitbook
 #' }
-thesis_gitbook <- function(...){
-
+thesis_gitbook <- function(...) {
   config_default <- list(
-    toc = list(collapse = "section",
-               before = '<li><a href="./"></a></li>',
-               after = '<li><a href="https://github.com/rstudio/bookdown" target="blank">Published with bookdown</a></li>'))
+    toc = list(
+      collapse = "section",
+      before = '<li><a href="./"></a></li>',
+      after = paste(
+        '<li><a href="https://github.com/rstudio/bookdown',
+        'target="blank">Published with bookdown</a></li>'
+      )
+    )
+  )
 
   listarg <- list(...)
 
@@ -89,7 +97,6 @@ thesis_gitbook <- function(...){
   base$knitr$opts_chunk$fig.align <- "center"
 
   base
-
 }
 
 #' Creates an R Markdown Word Thesis document
@@ -104,10 +111,9 @@ thesis_gitbook <- function(...){
 #' the Reed Senior Thesis Word template
 #' @examples
 #' \dontrun{
-#'  output: thesisdown::thesis_word
+#' output:thesisdown::thesis_word
 #' }
-thesis_word <- function(...){
-
+thesis_word <- function(...) {
   base <- bookdown::word_document2(...)
 
   # Mostly copied from knitr::render_sweave
@@ -115,7 +121,6 @@ thesis_word <- function(...){
   base$knitr$opts_chunk$fig.align <- "center"
 
   base
-
 }
 
 #' Creates an R Markdown epub Thesis document
@@ -129,10 +134,9 @@ thesis_word <- function(...){
 #' @return A ebook version of the thesis
 #' @examples
 #' \dontrun{
-#'  output: thesisdown::thesis_epub
+#' output:thesisdown::thesis_epub
 #' }
-thesis_epub <- function(...){
-
+thesis_epub <- function(...) {
   base <- bookdown::epub_book(...)
 
   # Mostly copied from knitr::render_sweave
@@ -140,17 +144,21 @@ thesis_epub <- function(...){
   base$knitr$opts_chunk$fig.align <- "center"
 
   base
-
 }
 
-fix_envs = function(x){
-  beg_reg <- '^\\s*\\\\begin\\{.*\\}'
-  end_reg <- '^\\s*\\\\end\\{.*\\}'
-  i3 = if (length(i1 <- grep(beg_reg, x))) (i1 - 1)[grepl("^\\s*$", x[i1 - 1])]
+fix_envs <- function(x) {
+  beg_reg <- "^\\s*\\\\begin\\{.*\\}"
+  end_reg <- "^\\s*\\\\end\\{.*\\}"
+  i3 <- if (length(i1 <- grep(beg_reg, x))) {
+    (i1 - 1)[grepl("^\\s*$", x[i1 - 1])]
+  }
 
-  i3 = c(i3,
-         if (length(i2 <- grep(end_reg, x))) (i2 + 1)[grepl("^\\s*$", x[i2 + 1])]
+  i3 <- c(
+    i3,
+    if (length(i2 <- grep(end_reg, x))) {
+      (i2 + 1)[grepl("^\\s*$", x[i2 + 1])]
+    }
   )
-  if (length(i3)) x = x[-i3]
+  if (length(i3)) x <- x[-i3]
   x
 }
