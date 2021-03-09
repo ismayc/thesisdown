c59e569bfd09177395e9d0a15de5480e1c3cf490
Chester Ismay <chester.ismay@datarobot.com>
2021-03-09 10:13:40 -0800

    OpenFiles works for RStudio 1.4.1106 or greater

--- R/.setup.R
+++ R/.setup.R
@@ -5,3 +5,5 @@ invisible(lapply(suggest_pkgs, usethis::use_package, type = "Suggests"))
 
 usethis::use_pkgdown()
 usethis::use_mit_license(name = "Chester Ismay")
+
+usethis::use_package("R.utils", type = "Suggests")
