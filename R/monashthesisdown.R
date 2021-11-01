31666f74225c2f699436e8f67d5d59a96fff266d
Mushfiqul Anwar Siraji <88833854+masiraji@users.noreply.github.com>
2021-11-02 01:52:44 +0800

    Description updated

---
+++ R/monashthesisdown.R
@@ -0,0 +1,12 @@
+#' monashthesisdown: A package for creating undergrad and post-grad theses using RMarkdown
+#'
+#' This package inspired by thesisdown and  Prof’ Rob J Hyndman’s work, uses bookdown package  knits your thesis in pdf, gitbook and epub format according to the guidelines of The Monash University
+#'
+#'
+#'
+#'It is advisable that you load **bookdown**, along with **monashthesisdown**
+#'
+#' @docType package
+#' @name monashthesisdown
+NULL
+#> NULL
